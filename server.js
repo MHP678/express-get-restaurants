f576fbde0a9a39d1488b89600d21f620a67ac960
MHP678 <muhammadpatel465@gmail.com>
2023-02-20 16:28:54 +0000

    "Thumbs up"

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const app = express();
-const {Restaurant} = require("./models/index")
-const {sequelize} = require("./db");
+const { Restaurant } = require("./models/index");
+const { sequelize } = require("./db");
 
 const port = 3000;
 
-//TODO: Create your GET Request Route Below: 
-
+// Create a GET request route for /restaurants
+app.get('/restaurants', async (req, res) => {
+  try {
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+});
 
 app.listen(port, () => {
-    sequelize.sync();
-    console.log("Your server is listening on port " + port);
-})
+  sequelize.sync();
+  console.log("Your server is listening on port " + port);
+});
